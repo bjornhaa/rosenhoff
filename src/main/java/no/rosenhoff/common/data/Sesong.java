d48c6324adbdca476b298409f2f48ec9a2dc8fc1
Bjørn Haakenstad <bjorn@haakenstad.com>
2011-10-21 20:58:53 +0200

    la til tidligere sesonger

--- src/main/java/no/rosenhoff/common/data/Sesong.java
+++ src/main/java/no/rosenhoff/common/data/Sesong.java
@@ -1,6 +1,15 @@
 package no.rosenhoff.common.data;
 
 public enum Sesong {
-    SESONG_2008, SESONG_2009, SESONG_2010, SESONG_2011, SESONG_2012, SESONG_2013
+    SESONG_2008("2008/2009"), SESONG_2009("2009/2010"), SESONG_2010("2010/2011"), SESONG_2011("2011/2012"), SESONG_2012("2012/2013"), SESONG_2013("2013/2014");
 
+    private final String displayText;
+
+    Sesong(String displayText) {
+        this.displayText = displayText;
+    }
+
+    public String getDisplayText() {
+        return displayText;
+    }
 }
