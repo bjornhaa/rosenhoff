ba1807ec404e7e6e2ec7002bfcb829fadd62e5d8
Bjørn Haakenstad <bjorn@haakenstad.com>
2013-09-17 23:29:02 +0200

    ny sesong

--- src/main/java/no/rosenhoff/common/data/Sesong.java
+++ src/main/java/no/rosenhoff/common/data/Sesong.java
@@ -6,7 +6,8 @@ import java.util.List;
 public enum Sesong {
     SESONG_2009("2010/2011"), // en liten bug
     SESONG_2011("2011/2012"),
-    SESONG_2012("2012/2013");
+    SESONG_2012("2012/2013"),
+    SESONG_2013("2013/2014");
 
     private final String displayText;
 
