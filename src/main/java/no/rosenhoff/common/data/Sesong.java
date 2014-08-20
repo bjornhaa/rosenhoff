102eb14783337da5a212704040ad86c7d73091d3
Bjørn Haakenstad <bjorn@haakenstad.com>
2014-08-20 18:32:45 +0200

    oppdaterte sesong til 2014/2015

--- src/main/java/no/rosenhoff/common/data/Sesong.java
+++ src/main/java/no/rosenhoff/common/data/Sesong.java
@@ -7,7 +7,8 @@ public enum Sesong {
     SESONG_2009("2010/2011"), // en liten bug
     SESONG_2011("2011/2012"),
     SESONG_2012("2012/2013"),
-    SESONG_2013("2013/2014");
+    SESONG_2013("2013/2014"),
+    SESONG_2014("2014/2015");
 
     private final String displayText;
 
