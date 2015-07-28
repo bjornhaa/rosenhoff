a0a3acc6ea73d080e9674ec9ae975dea10f81ed4
bjornhaa <t746301@TNA-07-5001.corp.telenor.no>
2015-07-28 11:34:09 +0200

    ny sesong 2015-2016

--- src/main/java/no/rosenhoff/common/data/Sesong.java
+++ src/main/java/no/rosenhoff/common/data/Sesong.java
@@ -8,7 +8,8 @@ public enum Sesong {
     SESONG_2011("2011/2012"),
     SESONG_2012("2012/2013"),
     SESONG_2013("2013/2014"),
-    SESONG_2014("2014/2015");
+    SESONG_2014("2014/2015"),
+    SESONG_2015("2015/2016");
 
     private final String displayText;
 
