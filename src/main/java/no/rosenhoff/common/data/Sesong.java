c64ceeef51547b7c823090cd50f8235d518b6f95
bjornhaa <bjorn@haakenstad.com>
2019-11-02 19:10:54 +0100

    ny sesong

--- src/main/java/no/rosenhoff/common/data/Sesong.java
+++ src/main/java/no/rosenhoff/common/data/Sesong.java
@@ -12,7 +12,11 @@ public enum Sesong {
     SESONG_2015("2015/2016"),
     SESONG_2016("2016/2017"),
     SESONG_2017("2017/2018"),
-    SESONG_2018("2018/2019");
+    SESONG_2018("2018/2019"),
+    SESONG_2019("2019/2020"),
+    SESONG_2020("2020/2022"),
+    SESONG_2021("2021/2022"),
+    SESONG_2022("2022/2023");
 
     private final String displayText;
 
